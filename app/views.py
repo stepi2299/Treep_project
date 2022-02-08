cdabda9478e13589190ed51037e73b866c819c7b
stepel <boenisch22@gmail.com>
2022-02-08 18:08:15 +0100

    login view

--- app/views.py
+++ app/views.py
@@ -15,24 +15,19 @@ def index():
     return "Hello World"
 
 
-@flask_app.route("/login", methods=["GET", "POST"])
+@flask_app.route("/login", methods=["POST"], strict_slashes=False)
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = AppUser.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash("Invalid username or password")
-            return redirect(url_for("login"))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get("next")
-        if not next_page or url_parse(next_page).netloc != "":
-            next_page = url_for("index")
-        return redirect(url_for("index"))
-    return render_template(
-        "login.html", title="Sign In", form=form
-    )  #  not template but json
+        return jsonify({'result': False})
+    username = request.json['username']
+    password = request.json['body']
+
+    user = AppUser.query.filter_by(username=username).first()
+    if user is None or not user.check_password(password):
+        return jsonify({'result': False})
+    else:
+        login_user(user)
+        return jsonify({'result': True})
 
 
 @flask_app.route("/logout")
